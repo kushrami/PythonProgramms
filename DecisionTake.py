e8f03aa4b4646b2a0bed5fe85c5b8588f0d21f14
kushrami <kushrami16@gmail.com>
2020-09-05 18:36:27 +0530

    If else statements programs

---
+++ DecisionTake.py
@@ -0,0 +1,29 @@
+# variable = input("Enter your input :")
+# if variable == "Yes":
+#     print("Something")
+# elif variable == "No":
+#     print("Nothing")
+# else:
+#     print("WOW")
+# print("Bye!")
+# deposit = int(input("Enter your amount = "))
+# if (deposit) > 100:
+#     freesurprise = True
+# else :
+#     freesurprise = False
+
+# if freesurprise == True:
+#     print("you won a surprise")
+# print("Happy Shopping!")
+
+
+Likewise = int(input("Enter a number in 1 to 10"))
+
+if Likewise == 3 and Likewise%3 == 0:
+    print("There is good 3 in it")
+elif Likewise == 2 or Likewise%2 == 0:
+    print("This number is either 2 or divisible by 2")
+
+else :
+    print("Haha its 1.")
+#solve above one problem.
