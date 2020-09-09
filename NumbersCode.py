4820eaf1192d953fa872d46d7e2d47a1505081df
kushrami <kushrami16@gmail.com>
2020-09-09 20:23:02 +0530

    Numbers in python, introduction and Basic

---
+++ NumbersCode.py
@@ -0,0 +1,24 @@
+area = 0
+width = 10
+height = 12
+
+area = height * width / 2
+
+#printing formatted float value with 2 decimal value
+print("Total area:%.2f" % area)
+
+NewVar = 12
+#printing formatted decimal value with tab space.
+print("New variable decimal value = %4d" % NewVar)
+
+print("New variable decimal value = %04d" % NewVar)
+
+#print formatted string with difffrent
+print("New variable decimal value ={0:.2f} ".format(NewVar))
+print("New variable decimal value ={0:f} and {1:01d}".format(NewVar,2345))
+
+
+
+
+
+
